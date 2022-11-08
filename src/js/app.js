c862d312945a676499eb2788a4b5597d15858e70
Муравьёв Д.С <C:\Users\muraviev\AppData\Roaming\The Bat!>
2022-11-08 17:17:19 +0300

    0811 1717

--- src/js/app.js
+++ src/js/app.js
@@ -33,3 +33,25 @@ if (document.querySelector('#to_signup')) {
         document.querySelector('.modal-signin').classList.remove('active');
     }, false);
 }
+document.getElementById("defaultOpen").click();
+
+function openTab(evt, cityName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcont");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
